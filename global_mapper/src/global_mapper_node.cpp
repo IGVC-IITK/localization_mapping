29dc1ab679f1393a3827fafaccca5c173c047234
Nitik Jain <39531001+nitik1998@users.noreply.github.com>
2019-01-22 19:15:37 +0530

    Modified global_mapper package for better image integration
    Now stores two sperate maps which finally merge for final occupancy grid

--- global_mapper/src/global_mapper_node.cpp
+++ global_mapper/src/global_mapper_node.cpp
@@ -1,23 +1,18 @@
 #include <ros/ros.h>
 #include <stdlib.h>
+#include <vector>
 #include <nav_msgs/OccupancyGrid.h>
-#include "cv_bridge/cv_bridge.h"
-#include "image_transport/image_transport.h"
-#include "opencv2/core/core.hpp"
-#include "opencv2/highgui/highgui.hpp"
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <geometry_msgs/TransformStamped.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/transform_listener.h>
 #include <sensor_msgs/LaserScan.h>
 #include <nav_msgs/Odometry.h>
-
-#define cellResolution 0.5 //(meters/cell)
-#define real_map_width 200
-#define real_map_height 200
-float map_origin_position[3] = {-200,-200,0};
-#define image_scale 1	 //(pixels/meter)
-#define mapOriginToImageX 100 //cell no. of pixel
-#define mapOriginToImageY 100 // at (x,y)===(columns/2,rows)
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
 
 using namespace std;
 
@@ -30,12 +25,29 @@ private:
   
   geometry_msgs::TransformStamped lidar_to_map, cam_to_map;
   nav_msgs::OccupancyGrid real_map;
+  std::vector<char> real_laser, real_lanes;
+
+  //getting parameters
+  double cellResolution = 0.05, image_scale = 160.0;
+  int real_map_width_m = 200, real_map_height_m = 200;
+
+  //calculating required parameters
+  int real_map_width = (int)(real_map_width_m/cellResolution);
+  int real_map_height = (int)(real_map_height_m/cellResolution);
+  double map_origin_position[3] = {-real_map_width_m/2,-real_map_height_m/2,0};
 
 public:
   tf2_ros::Buffer tfBuffer;
     tf2_ros::TransformListener* tfListener;
   real_mapper(){
   
+  	nh_.param("cellResolution", cellResolution, 0.05);
+  	nh_.param("real_map_width_m", real_map_width_m, 200);
+  	nh_.param("real_map_height_m", real_map_height_m, 200);
+  	nh_.param("image_scale", image_scale, 160.0);
+
+	
+
   real_map.header.frame_id = "odom";
     real_map.header.stamp = ros::Time::now();
     real_map.info.resolution = cellResolution;
@@ -46,29 +58,31 @@ public:
     tfListener = new tf2_ros::TransformListener(tfBuffer);
     pub_ = nh_.advertise<nav_msgs::OccupancyGrid>("/real_map", 1);
     sub1_ = nh_.subscribe<sensor_msgs::LaserScan>("/scan_1", 1, &real_mapper::scanCallback, this);
-    sub2_ = nh_.subscribe("/top_view", 100, &real_mapper::imageCallback, this);
-  ROS_INFO("Started real_ma2ppe2r");
+    sub2_ = nh_.subscribe("/top_view", 1, &real_mapper::imageCallback, this);
+  ROS_INFO("Started real_mapper");
   }
 
   void scanCallback(const sensor_msgs::LaserScanConstPtr& scan_msg){
-    ROS_INFO("Subscribing to scanCallback");
-    lidar_to_map = this->tfBuffer.lookupTransform("odom", "laser_1", ros::Time(0));
+    
+    lidar_to_map = tfBuffer.lookupTransform("odom", "laser_front", ros::Time(0));
     geometry_msgs::PointStamped point_lidar, point_robot;
     int x,y;
     ROS_INFO("moving to outer loop");
     for(int i=0;i<=(int)((scan_msg->angle_max - scan_msg->angle_min)/scan_msg->angle_increment);i++){
-      float laser_angle = scan_msg->angle_min + scan_msg->angle_increment*i;
+      double laser_angle = scan_msg->angle_min + scan_msg->angle_increment*i;
       if((laser_angle>-M_PI/2 && laser_angle<M_PI) || scan_msg->ranges[i] < scan_msg->range_min || scan_msg->ranges[i] > scan_msg->range_max)
         continue;
-      for(float range_current = 0.0; range_current<scan_msg->ranges[i]; range_current+=cellResolution){
-        point_lidar.point.x = scan_msg->ranges[i]*cos(laser_angle);
-        point_lidar.point.y = scan_msg->ranges[i]*sin(laser_angle);
+      for(double range_current = 0.0; range_current<scan_msg->ranges[i]; range_current+=cellResolution){
+        point_lidar.point.x = range_current*cos(laser_angle);
+        point_lidar.point.y = range_current*sin(laser_angle);
         point_lidar.point.z = 0.0;
         tf2::doTransform(point_lidar, point_robot, lidar_to_map);
         x = (int)((point_robot.point.x - map_origin_position[0])/cellResolution);
         y = (int)((point_robot.point.y - map_origin_position[1])/cellResolution);
-        if ((0 < y*real_map_width + x) && (y*real_map_width + x < real_map_width*real_map_height))
-          real_map.data[y*real_map_width + x] = 0;
+        
+        if ((0 <= y )&&(y < real_map_height)&&(0 <= x)&&(x < real_map_width))
+          real_laser[y*real_map_width + x] = 0;
+
       }
       point_lidar.point.x = scan_msg->ranges[i]*cos(laser_angle);
       point_lidar.point.y = scan_msg->ranges[i]*sin(laser_angle);
@@ -76,54 +90,64 @@ public:
       tf2::doTransform(point_lidar, point_robot, lidar_to_map);
       x = (int)((point_robot.point.x - map_origin_position[0])/cellResolution);
       y = (int)((point_robot.point.y - map_origin_position[1])/cellResolution);
-      if ((0 < y*real_map_width + x) && (y*real_map_width + x < real_map_width*real_map_height))
-        real_map.data[y*real_map_width + x] = 100;
+      if ((0 <= y )&&(y < real_map_height)&&(0 <= x)&&(x < real_map_width))
+        real_laser[y*real_map_width + x] = 100;
     }
   }
 
   void fillWithUnknown(){
+    real_lanes.resize(real_map_width*real_map_height);
+    real_laser.resize(real_map_width*real_map_height);
     real_map.data.resize(real_map_width*real_map_height);
      for(int i=0;i<real_map_width*real_map_height;i++){
+      real_laser[i] = -1;
+      real_lanes[i] = -1;
       real_map.data[i] = -1;
      }
   }
 
   void imageCallback(const sensor_msgs::ImageConstPtr& msg){
-    //cam_to_map = this->tfBuffer.lookupTransform("odom", msg->header.frame_id, ros::Time(0));
-    //geometry_msgs::PointStamped point_cam, point_robot;
+    cam_to_map = tfBuffer.lookupTransform("odom", msg->header.frame_id, ros::Time(0));
+    geometry_msgs::PointStamped point_cam, point_robot;
     int x,y;
-    //bool right_flag = false,left_flag = false;
-    cv::Mat gs = cv_bridge::toCvShare(msg, "8UC1")->image;
-    ROS_INFO("moving to image loop %d,%d",gs.rows,gs.cols);
+    cv::Mat gs = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::MONO8)->image;
+   
     for(int i=0;i<gs.rows;i++)
-      {
+    {
       for(int j=0;j<gs.cols;j++)
       {
-        // point_cam.point.x = (gs.rows-1-i)/image_scale*cellResolution;
-        // point_cam.point.y = j/image_scale*cellResolution;
-        // point_cam.point.z = 0.0;
-        // tf2::doTransform(point_cam, point_robot, cam_to_map);
-        // x = (int)((point_robot.point.x - map_origin_position[0])/cellResolution);
-        // y = (int)((point_robot.point.y - map_origin_position[1])/cellResolution);
-        x=(gs.rows-1-i)/image_scale*cellResolution;
-        y=j/image_scale*cellResolution;
-        if ((0 < y*real_map_width + x) && (y*real_map_width + x < real_map_width*real_map_height)){
-        // if ((bool)gs.at<uchar>(j, i) && (x != i && y != j)){
-          if (gs.at<int>(i,j) == 0)
-            real_map.data[real_map_height*y + x] = 0;
-          else
-            real_map.data[real_map_height*y + x] = 100;
+        point_cam.point.x = j/(double)image_scale;
+        point_cam.point.y = i/(double)image_scale;
+        point_cam.point.z = 0.0;
+        tf2::doTransform(point_cam, point_robot, cam_to_map);
+        x = (int)((point_robot.point.x - map_origin_position[0])/cellResolution);
+        y = (int)((point_robot.point.y - map_origin_position[1])/cellResolution);
+        if ((0 <= y )&&(y < real_map_height)&&(0 <= x)&&(x < real_map_width)){
+        	if (gs.at<uchar>(i, j))
+        	{
+            	real_lanes[real_map_width*y + x] = 100;
+        	}
+            	
         }
-        //if(j/(image_scale*cellResolution)<real_map_width && ((gs.rows-1-i)/(image_scale*cellResolution))<real_map_height)
-        //    if (gs.at<uchar>(i,j) == 0)
-        //    	real_map.data[real_map_height*(int)(j/(image_scale*cellResolution)) + (int)((gs.rows-1-i)/(image_scale*cellResolution))] = 0;
-        //    else
-        //    	real_map.data[real_map_height*(int)(j/(image_scale*cellResolution)) + (int)((gs.rows-1-i)/(image_scale*cellResolution))] = 100;
-        }
+      }
     }
   }
 
   void publishMap(){
+  	for(int y=0; y<real_map_height; y++)
+  	{
+  	  for(int x=0; x<real_map_width; x++)
+  	  {
+  	  	  if(real_lanes[real_map_width*y + x] == 100)
+  	  	  {
+	  	    real_map.data[real_map_width*y + x] = 100;	
+  	  	  }
+  	  	  else
+  	  	  {
+  	  	  	real_map.data[real_map_width*y + x] = real_laser[real_map_width*y + x];
+  	  	  }	
+  	  }		
+  	}
     pub_.publish(real_map);
   }
 
@@ -140,6 +164,7 @@ int main(int argc, char** argv)
     if(loop_count%30==0){
       loop_count=0;
       sub_and_pub.publishMap();
+
     }
     ros::spinOnce();
     loop_rate.sleep();
