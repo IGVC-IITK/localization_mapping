39e5aa9f01f7c37bd203510fff424de247f85df6
Nitik Jain <39531001+nitik1998@users.noreply.github.com>
2019-04-17 11:59:38 +0530

    Fixed crashing of global_mapper
    Added try-catch to look for transform and wait until one is found. Previously the node used to crash when ni transform was found, now it gives an error message

--- global_mapper/src/global_mapper_node.cpp
+++ global_mapper/src/global_mapper_node.cpp
@@ -39,10 +39,10 @@ public:
   real_mapper()
   {
     // getting parameters
-  	nh_.param("cellResolution", cellResolution, 0.05);
-  	nh_.param("real_map_width_m", real_map_width_m, 40.0);
-  	nh_.param("real_map_height_m", real_map_height_m, 40.0);
-  	nh_.param("image_scale", image_scale, 100.0);
+    nh_.param("cellResolution", cellResolution, 0.05);
+    nh_.param("real_map_width_m", real_map_width_m, 40.0);
+    nh_.param("real_map_height_m", real_map_height_m, 40.0);
+    nh_.param("image_scale", image_scale, 100.0);
 
     // calculating dependent parameters
     real_map_width = (int)(real_map_width_m/cellResolution);
@@ -67,34 +67,39 @@ public:
 
   void scanCallback(const sensor_msgs::LaserScanConstPtr& scan_msg){
     
-    lidar_to_map = tfBuffer.lookupTransform("odom", "laser_front", ros::Time(0));
-    geometry_msgs::PointStamped point_lidar, point_robot;
-    int x,y;
-    ROS_INFO("moving to outer loop");
-    for(int i=0;i<=(int)((scan_msg->angle_max - scan_msg->angle_min)/scan_msg->angle_increment);i++){
-      double laser_angle = scan_msg->angle_min + scan_msg->angle_increment*i;
-      if((laser_angle>-M_PI/2 && laser_angle<M_PI) || scan_msg->ranges[i] < scan_msg->range_min || scan_msg->ranges[i] > scan_msg->range_max)
-        continue;
-      for(double range_current = 0.0; range_current<scan_msg->ranges[i]; range_current+=cellResolution){
-        point_lidar.point.x = range_current*cos(laser_angle);
-        point_lidar.point.y = range_current*sin(laser_angle);
+    try{
+      lidar_to_map = tfBuffer.lookupTransform("odom", "laser_front", ros::Time(0));
+      geometry_msgs::PointStamped point_lidar, point_robot;
+      int x,y;
+      ROS_INFO("moving to outer loop");
+      for(int i=0;i<=(int)((scan_msg->angle_max - scan_msg->angle_min)/scan_msg->angle_increment);i++){
+        double laser_angle = scan_msg->angle_min + scan_msg->angle_increment*i;
+        if((laser_angle>-M_PI/2 && laser_angle<M_PI) || scan_msg->ranges[i] < scan_msg->range_min || scan_msg->ranges[i] > scan_msg->range_max)
+          continue;
+        for(double range_current = 0.0; range_current<scan_msg->ranges[i]; range_current+=cellResolution){
+          point_lidar.point.x = range_current*cos(laser_angle);
+          point_lidar.point.y = range_current*sin(laser_angle);
+          point_lidar.point.z = 0.0;
+          tf2::doTransform(point_lidar, point_robot, lidar_to_map);
+          x = (int)((point_robot.point.x - map_origin_position[0])/cellResolution);
+          y = (int)((point_robot.point.y - map_origin_position[1])/cellResolution);
+          
+          if ((0 <= y )&&(y < real_map_height)&&(0 <= x)&&(x < real_map_width))
+            real_laser[y*real_map_width + x] = 0;
+
+        }
+        point_lidar.point.x = scan_msg->ranges[i]*cos(laser_angle);
+        point_lidar.point.y = scan_msg->ranges[i]*sin(laser_angle);
         point_lidar.point.z = 0.0;
         tf2::doTransform(point_lidar, point_robot, lidar_to_map);
         x = (int)((point_robot.point.x - map_origin_position[0])/cellResolution);
         y = (int)((point_robot.point.y - map_origin_position[1])/cellResolution);
-        
         if ((0 <= y )&&(y < real_map_height)&&(0 <= x)&&(x < real_map_width))
-          real_laser[y*real_map_width + x] = 0;
-
+          real_laser[y*real_map_width + x] = 100;
       }
-      point_lidar.point.x = scan_msg->ranges[i]*cos(laser_angle);
-      point_lidar.point.y = scan_msg->ranges[i]*sin(laser_angle);
-      point_lidar.point.z = 0.0;
-      tf2::doTransform(point_lidar, point_robot, lidar_to_map);
-      x = (int)((point_robot.point.x - map_origin_position[0])/cellResolution);
-      y = (int)((point_robot.point.y - map_origin_position[1])/cellResolution);
-      if ((0 <= y )&&(y < real_map_height)&&(0 <= x)&&(x < real_map_width))
-        real_laser[y*real_map_width + x] = 100;
+    }
+    catch (tf2::TransformException &ex){
+      ROS_WARN_THROTTLE(1.0, "Could not get transform: %s", ex.what());
     }
   }
 
@@ -110,47 +115,51 @@ public:
   }
 
   void imageCallback(const sensor_msgs::ImageConstPtr& msg){
-    cam_to_map = tfBuffer.lookupTransform("odom", msg->header.frame_id, ros::Time(0));
-    geometry_msgs::PointStamped point_cam, point_robot;
-    int x,y;
-    cv::Mat gs = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::MONO8)->image;
-   
-    for(int i=0;i<gs.rows;i++)
-    {
-      for(int j=0;j<gs.cols;j++)
+    try{
+      cam_to_map = tfBuffer.lookupTransform("odom", msg->header.frame_id, ros::Time(0));
+      geometry_msgs::PointStamped point_cam, point_robot;
+      int x,y;
+      cv::Mat gs = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::MONO8)->image;
+     
+      for(int i=0;i<gs.rows;i++)
       {
-        point_cam.point.x = j/(double)image_scale;
-        point_cam.point.y = i/(double)image_scale;
-        point_cam.point.z = 0.0;
-        tf2::doTransform(point_cam, point_robot, cam_to_map);
-        x = (int)((point_robot.point.x - map_origin_position[0])/cellResolution);
-        y = (int)((point_robot.point.y - map_origin_position[1])/cellResolution);
-        if ((0 <= y )&&(y < real_map_height)&&(0 <= x)&&(x < real_map_width)){
-        	if (gs.at<uchar>(i, j))
-        	{
-            	real_lanes[real_map_width*y + x] = 100;
-        	}
-            	
+        for(int j=0;j<gs.cols;j++)
+        {
+          point_cam.point.x = j/(double)image_scale;
+          point_cam.point.y = i/(double)image_scale;
+          point_cam.point.z = 0.0;
+          tf2::doTransform(point_cam, point_robot, cam_to_map);
+          x = (int)((point_robot.point.x - map_origin_position[0])/cellResolution);
+          y = (int)((point_robot.point.y - map_origin_position[1])/cellResolution);
+          if ((0 <= y )&&(y < real_map_height)&&(0 <= x)&&(x < real_map_width)){
+            if (gs.at<uchar>(i, j))
+            {
+                real_lanes[real_map_width*y + x] = 100;
+            }   
+          }
         }
       }
+    }
+    catch (tf2::TransformException &ex){
+      ROS_WARN_THROTTLE(1.0, "Could not get transform: %s", ex.what());
     }
   }
 
   void publishMap(){
-  	for(int y=0; y<real_map_height; y++)
-  	{
-  	  for(int x=0; x<real_map_width; x++)
-  	  {
-  	  	  if(real_lanes[real_map_width*y + x] == 100)
-  	  	  {
-	  	    real_map.data[real_map_width*y + x] = 100;	
-  	  	  }
-  	  	  else
-  	  	  {
-  	  	  	real_map.data[real_map_width*y + x] = real_laser[real_map_width*y + x];
-  	  	  }	
-  	  }		
-  	}
+    for(int y=0; y<real_map_height; y++)
+    {
+      for(int x=0; x<real_map_width; x++)
+      {
+          if(real_lanes[real_map_width*y + x] == 100)
+          {
+          real_map.data[real_map_width*y + x] = 100;  
+          }
+          else
+          {
+            real_map.data[real_map_width*y + x] = real_laser[real_map_width*y + x];
+          } 
+      }   
+    }
     pub_.publish(real_map);
   }
 
